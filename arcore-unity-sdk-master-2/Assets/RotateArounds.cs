af2553eb865936ad1a86827a008e7596e9bba991
drachmat <drachmat.iskandar@gmail.com>
2019-01-24 18:33:01 +0700

    First Commit

---
+++ arcore-unity-sdk-master-2/Assets/RotateArounds.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RotateArounds : MonoBehaviour {
+
+    public float speed = 1f;
+    public GameObject ObjectToRotate;
+
+    public void RotateMyObject()
+    {
+        float sliderValue = GetComponent<Slider>().value;
+        ObjectToRotate.transform.rotation = Quaternion.Euler(sliderValue * 360, 0, 90);
+    }
+}
---
+++ arcore-unity-sdk-master-2/Assets/rotations.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotations : MonoBehaviour {
+
+    public float speed;
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * speed);
+    }
+}
